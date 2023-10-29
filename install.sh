8a22981f7352c38f053459a0db5cee18b48772ab
Nestor Jaraba <nestorjari784@gmail.com>
2023-10-28 23:54:44 -0500

    x

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+#
 master=$(dirname $(readlink -fm "$0"))
-exec ${master}/sh/yay-firmware.sh
-exec ${master}/sh/fonts.sh
-exec ${master}/sh/xorg.sh
+#ln -sf
+#yes | cp -rf
+
+sudo yes | cp -rf ${master}/etc/00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
+sudo yes | cp -rf ${master}/etc/20-amdgpu.conf /etc/X11/xorg.c/onf.d/20-amdgpu.conf
+sudo cp -fv /etc/X11/xinit/xinitrc ${HOME}/.xinitrc
+echo -e "exec qtile start" >> ${HOME}/.xinitrc
+sudo chown -R nestor:users ${HOME}/.xinitrc
+sudo chown -R nestor:users ${HOME}/.config
+
+yay -S - < ${master}/etc/pkgdistro.txt
