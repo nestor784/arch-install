1068e0c630e1fe0f9b1d6229565f470fcacf79b1
Nestor Jaraba <nestorjari784@gmail.com>
2023-10-29 03:18:28 -0500

    x

--- sh/distro.sh
+++ sh/distro.sh
@@ -4,7 +4,7 @@ dotfiles="$HOME/dotfiles"
 DIR_QTILE="$HOME/.config/qtile/"
 DIR_ROFI="$HOME/.config/rofi/"
 
-git clone https://github.com/nestor784/dotfiles.git ${dotfiles}
+git clone --depth=1 https://github.com/nestor784/dotfiles.git ${dotfiles}
 
 # QTILE
 if [ -d "$DIR_QTILE" ];then
@@ -25,3 +25,18 @@ else
 fi
 fc-cache
 cp -r ${HOME}/dotfiles/.config/rofi/  ${DIR_ROFI}
+
+# GRUB
+git clone --depth=1 https://github.com/vinceliuice/grub2-themes ${HOME}/grub2-themes
+cp -rf ${master}/files/grub/background.jpg ${HOME}/grub2-themes/background.jpg
+cd ${HOME}/grub2-themes
+sudo ./install.sh -s 1080p -t vimix -i white
+vbeinfo=$(xdpyinfo | awk '/dimensions/{print $2}')
+sudo sed -i "/GRUB_GFXMODE/ s/1920x1080,auto/$vbeinfo,auto/" /etc/default/grub
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# SDDM
+yay -S sddm-sugar-dark
+sddm --example-config > ${HOME}/tmpfile
+sudo mv ${HOME}/tmpfile /etc/sddm.conf
+sudo sed -i "/Current=/ s/=/=sugar-dark/" /etc/sddm.conf
