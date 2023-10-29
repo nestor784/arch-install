eb8686bf55c4e4b2cf902ec781d352c0136ec0cd
Nestor Jaraba <nestorjari784@gmail.com>
2023-10-28 21:06:37 -0500

    x

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
-pacman -S - < ./pkgfirmware.txt
+#!/bin/bash
+
+errormsg="Ups... something was wrong."
+continuemsg="Press any key to resume"
+
+# YAY
+git clone https://aur.archlinux.org/yay.git ${HOME}/yay
+chown ${USER} ~/yay/
+cd ${HOME}/yay
+makepkg -si || echo "$errormsg"
+
+read -p "$continuemsg"
+
+yay -S - < ./pkgfirmware.txt
