56e1da05ef073cb09cc8ddb322247be54089f2e2
Nestor Jaraba <nestorjari784@gmail.com>
2023-10-29 03:33:02 -0500

    x

--- sh/distro.sh
+++ sh/distro.sh
@@ -10,7 +10,7 @@ git clone --depth=1 https://github.com/nestor784/dotfiles.git ${dotfiles}
 if [ -d "$DIR_QTILE" ];then
 	rm -rf .config/qtile/*
 fi
-cp -r ${HOME}/dotfiles/.config/qtile/ ${DIR_QTILE}
+cp -rf ${HOME}/dotfiles/.config/qtile/ ${DIR_QTILE}
 
 # ROFI
 if [ -d "$DIR_ROFI" ];then
@@ -24,14 +24,14 @@ else
 	cp -rf ${master}/files/rofi/fonts/* ${FONT_DIR}
 fi
 fc-cache
-cp -r ${HOME}/dotfiles/.config/rofi/  ${DIR_ROFI}
+cp -rf ${HOME}/dotfiles/.config/rofi/  ${DIR_ROFI}
 
 # GRUB
 git clone --depth=1 https://github.com/vinceliuice/grub2-themes ${HOME}/grub2-themes
 cp -rf ${master}/files/grub/background.jpg ${HOME}/grub2-themes/background.jpg
 cd ${HOME}/grub2-themes
 sudo ./install.sh -s 1080p -t vimix -i white
-vbeinfo=$(xdpyinfo | awk '/dimensions/{print $2}')
+vbeinfo="1366x768"
 sudo sed -i "/GRUB_GFXMODE/ s/1920x1080,auto/$vbeinfo,auto/" /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
 
@@ -40,3 +40,4 @@ yay -S sddm-sugar-dark
 sddm --example-config > ${HOME}/tmpfile
 sudo mv ${HOME}/tmpfile /etc/sddm.conf
 sudo sed -i "/Current=/ s/=/=sugar-dark/" /etc/sddm.conf
+sudo systemctl enable sddm.service
